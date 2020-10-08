dbf9286be651c53f17796c819189f5d80aa11c7a
karashiiro <49822414+karashiiro@users.noreply.github.com>
2020-10-08 11:09:29 -0700

    fix resolve order

--- src/services/api/universalis/Universalis.ts
+++ src/services/api/universalis/Universalis.ts
@@ -1,4 +1,4 @@
-import { RestClient } from "typed-rest-client";
+import { IRestResponse, RestClient } from "typed-rest-client";
 import { SERVERS } from "../../../data/SERVERS";
 import { DataCenter } from "../../../models";
 import { buildQueryString } from "../../../util/url";
@@ -24,21 +24,14 @@ class Universalis {
 	}
 
 	async marketDataCenter(dataCenter: DataCenter, itemId: number): Promise<MarketDataWorld[]> {
-		const data: MarketDataWorld[] = [];
-
-		const requests: Promise<void>[] = [];
+		const requests: Promise<IRestResponse<MarketDataWorld>>[] = [];
 		const { worlds } = SERVERS.find((server) => server.dataCenter === dataCenter)!;
 		for (const world of worlds) {
-			requests.push(
-				(async () => {
-					const res = await this.rest.get<MarketDataWorld>(`/api/${world}/${itemId}`);
-					data.push(res.result!);
-				})(),
-			);
+			const request = this.rest.get<MarketDataWorld>(`/api/${world}/${itemId}`);
+			requests.push(request);
 		}
-		await Promise.all(requests);
 
-		return data;
+		return (await Promise.all(requests)).map((res) => res.result!);
 	}
 
 	async recentlyUpdated(): Promise<RecentlyUpdated> {
